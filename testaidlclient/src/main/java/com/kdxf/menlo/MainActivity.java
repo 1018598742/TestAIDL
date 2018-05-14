4c5eb87961b030497db1149f005f71629d1ec5c4
jiaolei <1018598742@qq.com>
2018-05-14 13:57:59 +0800

    this is a test about aidl

---
+++ testaidlclient/src/main/java/com/kdxf/menlo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kdxf.menlo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kdxf.menlo.testaidlclient.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ testaidlservice/src/main/java/com/kdxf/menlo/MainActivity.java
@@ -0,0 +1,16 @@
+
+package com.kdxf.menlo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kdxf.menlo.testaidlservice.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
